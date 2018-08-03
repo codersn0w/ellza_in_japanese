723bd85d624bcd93ce78cd3982f9a6bf4de532f3
ThunderRa1n <Podsn0w@gmail.com>
2018-08-03 19:18:41 +0900

    addded chatbot function

--- reply_generator.py
+++ reply_generator.py
@@ -3,6 +3,7 @@ from engines.random_ua import generate_ua
 from engines.google_xpath import gsearch
 from engines.bnews_xpath import bnsearch
 from engines.gimage_xpath import gisearch
+from engines.chatbot_engine import make_reply
 import re
 def generate_reply(query):
     if len(query) > 70:
@@ -15,7 +16,8 @@ def generate_reply(query):
                    '標準ではWeb検索モードになっています。<br>オプションとして、<br>'\
                    '・Wikipediaを検索する際は「w:」<br>・ニュースを検索する際は「n:」<br>'\
                    '・画像を検索する際は「i:」<br>・YouTubeを検索する際は「y:」<br>'\
-                   'を検索ワードの先頭に入れてください。<br>'\
+                   '・Ellza (Bot) と会話する際は「b:」<br>を検索ワードの先頭に入れてください。<br>'\
+                   '[<a href="https://blog.ellpedia.com/term_and_privacy_policy/" target="_blank">規約・プライバシーポリシー</a>]<br>'\
                    '[<a href="https://github.com/thunderra1n/ellza_in_japanese" target="_blank">ソースコード</a>]',
        }]
        return results
@@ -46,6 +48,11 @@ def generate_reply(query):
       ua = generate_ua()
       ggl = gsearch(query, ua)
       results = [{}] + ggl[1]
+      return results
+
+    elif re.match('b:', query):
+      query = query.lstrip('b:')
+      results = make_reply(query)
       return results
 
     else:
