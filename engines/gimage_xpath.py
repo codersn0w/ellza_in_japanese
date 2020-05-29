d20ba500abb1f7ca143e957c0613e67c139f6978
ThunderRa1n <Podsn0w@gmail.com>
2020-05-29 11:15:53 +0900

    Update gimage_xpath.py

--- engines/gimage_xpath.py
+++ engines/gimage_xpath.py
@@ -2,7 +2,6 @@
 import urllib.request as req
 import urllib.parse as up
 import lxml.html as lh
-import html
 from json import loads
 import re
 
@@ -11,30 +10,33 @@ def gi_search(query, ua):
     values = {
       'q': query,
     }
-    s_url = 'http://www.google.com/search?gl=us&hl=en&tbm=isch&gws_rd=cr&safe=active&pws=0&gbv=1&' + up.urlencode(values)
+    s_url = 'http://www.google.co.jp/search?tbm=isch&gws_rd=cr&safe=active&pws=0&gbv=1&' + up.urlencode(values)
     headers = {
           "User-Agent": ua,
           }
     request = req.Request(url=s_url, headers=headers)
     res = req.urlopen(request, timeout=10).read()
     root = lh.fromstring(res.decode('utf-8'))
-    image_xpath = root.xpath('//table[4]//a')
-    link_xpath = './@href'
-    thumb_xpath = './img/@src'
+    image_xpath = root.xpath('//td[@class="e3goi"]')
+    link_xpath = './/tr[1]/td/a/@href'
+    thumb_xpath = './/tr[1]/td/a/div/img/@src'
     results = []
-    rc = re.compile('&sa=U&ved=0.*')
+    rc = re.compile('&sa=U&ved=.*')
     num = 0
     for im in image_xpath:
-      if num <= 0:
+      if num <= 7:
         url = im.xpath(link_xpath)[0].lstrip('/url?q=')
         url = rc.sub('', url)
         thumb_src = im.xpath(thumb_xpath)[0]
-        d_url = url
+        if len(url) > 60:
+          d_url = url[:60] + '...'
+        else:
+          d_url = url
         img_src = url
-        results.append({'url': html.escape(url, quote=True),
-          'd_url': html.escape(d_url, quote=True),
-          'thumb_src': html.escape(thumb_src, quote=True),
-          'img_src': html.escape(img_src, quote=True),
+        results.append({'url': url,
+          'd_url': d_url,
+          'thumb_src': thumb_src,
+          'img_src': img_src,
           })
         num+=1
       else:
